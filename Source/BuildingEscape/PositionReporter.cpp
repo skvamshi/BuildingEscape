4470b4d9f2a0d65fca2d83cef1ecb6a66fac43a7
Krishna Saladi <skvamshi@gmail.com>
2020-04-05 14:17:54 +0530

    position reporter component tto chair and additional assets added to the level.

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PositionReport.h"
+#include "PositionReporter.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UPositionReporter::UPositionReporter()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,7 +16,7 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
@@ -26,7 +26,7 @@ void UPositionReport::BeginPlay()
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
